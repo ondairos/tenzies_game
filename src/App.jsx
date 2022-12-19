5ded62367459742942c0b51bce8e6b4d15f7b5fa
Ondairos <ondairos@gmail.com>
2022-12-19 13:16:16 +0200

    Tenzies Game React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>ok</p>
+    </div>
+  );
+}
+
+export default App;
